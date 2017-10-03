b869bd8eb519349a65d1cf9dd52ea0074ab0ca54
Jeffrey Williams <jeffreywilliams.b@gmail.com>
2017-10-02 19:31:20 -0500

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 rotation = "rot"
-string_input = "text" 
+string_input = "" 
 
 form = """
 <!DOCTYPE html>
@@ -33,7 +33,7 @@ form = """
     <form action='/encrypt' method='post'>
         <label for="rot">Rotate by:</label>
             
-        <input type="text" name="rot" value={1} />
+        <input type="text" name="rot" value="0" />
         
         <textarea name="text">{0}</textarea>
         <input type="submit"/> 
@@ -45,7 +45,7 @@ form = """
       <!-- create your form here -->
     </body>
 </html>
-""".format(string_input,rotation)
+"""
 
 
 
@@ -57,93 +57,18 @@ def encrypt():
 
     encrypt_text = rotate_string(string, rotation_input)
    
- 
+    encrypt_string = form.format(encrypt_text)
    
-    return  """
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-        
-    </head>
-    <body>
-    <form action='/encrypt' method='post'>
-        <label for="rot">Rotate by:</label>
-            
-        <input type="text" name="rot" />
-        
-        <textarea name="text">{0}</textarea>
-        <input type="submit"/> 
-        
-
-
-
-    </form>
-      <!-- create your form here -->
-    </body>
-</html>
-""".format(encrypt_text)
+    return encrypt_string
     #new_encrpytion = encrpyt_string
      
 @app.route("/")
 def index():
+
     encrypt_string = ""
-    rot = ""
     
-    return  """
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-        
-    </head>
-    <body>
-    <form action='/encrypt' method='post'>
-        <label for="rot">Rotate by:</label>
-            
-        <input type="text" name="rot" value={1} />
-        
-        <textarea name="text">{0}</textarea>
-        <input type="submit"/> 
-        
-
-
-
-    </form>
-      <!-- create your form here -->
-    </body>
-</html>
-""".format(encrypt_string, rot)    
+    
+    return form.format(encrypt_string)
 
 
  
